1847521cff1b27ff5e3577eb483c1369a7d30df5
khachung2004 <hungnkph35189@fpt.edu.vn>
2023-05-25 23:15:43 +0700

    updata

--- app/src/main/java/com/hungnk/assgd1_and101/MainActivity.java
+++ app/src/main/java/com/hungnk/assgd1_and101/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+
     }
 }
